cbef1484cbd9d0e42f722ac18c815d2d64dddf28
Gerald Onyango <futuregerald@ninjasitm.com>
2017-04-14 15:16:44 +0000

    fixing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,16 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-//var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 exports.modifyWebpackConfig = function(config, stage) {
   if(stage === 'build-html') {
-
-    config.plugin(CopyWebpackPlugin([
-          
-            // {output}/file/without/extension
-            {
-                from: 'asset/_redirects',
-                to: 'public/',
-                toType: '_redirects'
-            },
-            
-          
-        ], {
-            // By default, we only copy modified files during
-            // a watch or webpack-dev-server build. Setting this
-            // to `true` copies all files.
-            copyUnmodified: true
-        })
-    )
+    config.removeLoader('css')
+    config.loader('css', function(cfg) {
+      cfg.test = /\.css$/
+      cfg.loader = ExtractTextPlugin.extract('css?minimize')
+      return cfg
+    })
+    config.plugin('extract-css',
+                  ExtractTextPlugin,
+                  ["styles.css", { allChunks: true }])
   }
   return config
 }
