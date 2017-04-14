bec2195f2cedfba110431ef47850c8b63c48be02
Gerald Onyango <futuregerald@ninjasitm.com>
2017-04-14 18:00:09 +0000

    redirects might be working

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,10 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var path = require('path');
-//var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-exports.postBuild = function(pages, callback) {
+exports.modifyWebpackConfig = function(config, stage) {
 
-    new CopyWebpackPlugin([
-          
-            // {output}/file/without/extension
-            {
-                from: '/assets/redirects.txt',
-                to: 'public/_redirects',
-                toType: 'file'
-            },
-            
-          
-        ], {
-            // By default, we only copy modified files during
-            // a watch or webpack-dev-server build. Setting this
-            // to `true` copies all files.
-            copyUnmodified: true
-        })
-    
-  callback()
+  config.loader('file-loader', function(cfg) {
+    cfg.test = /\.(txt)$/
+    cfg.loader = 'file-loader?name=[name]&context=./public'
+    return cfg
+  })
+  return config
 }
