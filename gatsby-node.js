c6360e57ec9ca5879ea8d12e670a7d838b412518
Gerald Onyango <futuregerald@ninjasitm.com>
2017-04-14 16:15:36 +0000

    fixing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,17 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
 //var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 exports.modifyWebpackConfig = function(config, stage) {
+     context: path.join(__dirname, 'app'),
+    devServer: {
+        // This is required for older versions of webpack-dev-server
+        // if you use absolute 'to' paths. The path should be an
+        // absolute path to your build destination.
+        outputPath: path.join(__dirname, 'build')
+    }
   if(stage === 'build-html') {
+    
     return cfg
     config.plugin( new CopyWebpackPlugin([
           
